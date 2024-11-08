2c9689a35d2a8bf2964b57aef6137e554fe0429b
MarkGee45 <moel55.mg@gmail.com>
2024-11-08 11:46:38 -0800

    added event listener checked if clicked target is a link and toggled clicked link

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 
 // Part 1: Getting Started
 
-const mainEl = document.getElementsByTagName('main');
-mainEl[0].style.backgroundColor = 'var(--main-bg)';
+const mainEl = document.getElementsByTagName("main");
+mainEl[0].style.backgroundColor = "var(--main-bg)";
 
 const h1Main = document.createElement("h1");
 h1Main.textContent = `DOM Manipulation`;
@@ -15,36 +15,47 @@ mainEl[0].classList.add("flex-ctr");
 
 const topMenuEl = document.getElementById("top-menu");
 topMenuEl.style.height = "100%";
-topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
 topMenuEl.classList.add("flex-around");
 
 // Part 3: Adding Menu Buttons
 
 const menuLinks = [
-    {text: 'about', href: '/about'},
-    {text: 'catalog', href: '#', subLinks: [
-      {text: 'all', href: '/catalog/all'},
-      {text: 'top selling', href: '/catalog/top'},
-      {text: 'search', href: '/catalog/search'},
-    ]},
-    {text: 'orders', href: '#' , subLinks: [
-      {text: 'new', href: '/orders/new'},
-      {text: 'pending', href: '/orders/pending'},
-      {text: 'history', href: '/orders/history'},
-    ]},
-    {text: 'account', href: '#', subLinks: [
-      {text: 'profile', href: '/account/profile'},
-      {text: 'sign out', href: '/account/signout'},
-    ]},
-  ];
+  { text: "about", href: "/about" },
+  {
+    text: "catalog",
+    href: "#",
+    subLinks: [
+      { text: "all", href: "/catalog/all" },
+      { text: "top selling", href: "/catalog/top" },
+      { text: "search", href: "/catalog/search" },
+    ],
+  },
+  {
+    text: "orders",
+    href: "#",
+    subLinks: [
+      { text: "new", href: "/orders/new" },
+      { text: "pending", href: "/orders/pending" },
+      { text: "history", href: "/orders/history" },
+    ],
+  },
+  {
+    text: "account",
+    href: "#",
+    subLinks: [
+      { text: "profile", href: "/account/profile" },
+      { text: "sign out", href: "/account/signout" },
+    ],
+  },
+];
 
-
-// menuLinks.forEach(ele => {
-//     const navEl = document.createElement("a");
-//     navEl.setAttribute("href", ele.href);
-//     navEl.textContent = ele.text;
-//     topMenuEl.appendChild(navEl);
-// });
+menuLinks.forEach((ele) => {
+  const navEl = document.createElement("a");
+  navEl.setAttribute("href", ele.href);
+  navEl.textContent = ele.text;
+  topMenuEl.appendChild(navEl);
+});
 
 // ============ DOM Manipulation (Part Two) ============
 
@@ -53,8 +64,19 @@ const menuLinks = [
 const subMenuEl = document.getElementById("sub-menu");
 // console.log(subMenuEl)
 subMenuEl.style.height = "100%";
-subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)';
-subMenuEl.style.classList.add('flex-around');
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+subMenuEl.classList.add("flex-around");
 subMenuEl.style.position = "absolute";
 
-const topMenuLinks = topMenuEl.querySelectorAll('a');
+const topMenuLinks = topMenuEl.querySelectorAll("#top-menu a");
+// console.log(topMenuLinks)
+topMenuEl.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  const menuLinks = evt.target;
+  if (menuLinks.tagName !== "A") return;
+
+  topMenuLinks.forEach((a) => a.classList.remove("active"));
+  menuLinks.classList.add("active");
+
+  console.log(menuLinks.textContent);
+});
